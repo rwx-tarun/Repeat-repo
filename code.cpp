e32abe69395d3cb1d1d97c82b0d6427e36d1928a
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-09-04 01:07:24 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,26 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int solve(int a[], int n)
+int dp[100][100];
+int knapsackRec(int wt[], int pr[], int n, int W)
 {
-  sort(a, a + n);
-  int res = 0;
-  for (int i = n - 2; i >= 0; i = i - 1)
+  if (W == 0 || n == 0)
+    return 0;
+
+  if (W >= wt[n - 1])
   {
-    res = res + a[i + 1] - a[i];
+    return max(pr[n - 1] + knapsack(wt, pr, n - 1, W - wt[n - 1]), knapsack(wt, pr, n - 1, W));
   }
-  return res;
+  else
+  {
+    return knapsack(wt, pr, n - 1, W);
+  }
+}
+}
+
+int knapsackMem(int wt[], int pr[], int n, int W)
+{
+  if (W == 0 || n == 0)
+    return 0;
+  if (dp[n][W] != -1)
+    return dp[n][W];
+
+  if (W >= wt[n - 1])
+  {
+    return dp[n][W] = max(pr[n - 1] + knapsack(wt, pr, n - 1, W - wt[n - 1]), knapsack(wt, pr, n - 1, W));
+  }
+  else
+  {
+    return dp[n][W] = knapsack(wt, pr, n - 1, W);
+  }
+}
 }
 
 int main()
 {
   int n;
   cin >> n;
-  int a[n];
+  int wt[n], pr[n];
+  int W;
+  cin >> W;
   for (int i = 0; i < n; i++)
-  {
-    cin >> a[i];
-  }
-  cout << solve(a, n);
+    cin >> wt[i];
+  for (int i = 0; i < n; i++)
+    cin >> pr[i];
+
+  cout << knapsackRec(wt, pr, n, W);
+  memset(dp, -1, sizeof(dp));
+  cout << knapsackMem(wt, pr, n, W);
   return 0;
 }
